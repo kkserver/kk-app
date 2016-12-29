e45a95f93bce340b5fea1f2b683fd8668563ef7d
zhang hailong <hailongz@qq.com>
2016-12-29 13:55:47 +0800

    update

--- KKRemoteService.go
+++ KKRemoteService.go
@@ -1,10 +1,10 @@
 package kkapp
 
 import (
-	"encoding/json"
 	"errors"
 	"github.com/kkserver/kk-lib/app"
 	"github.com/kkserver/kk-lib/kk"
+	"github.com/kkserver/kk-lib/kk/json"
 	"time"
 )
 
@@ -26,14 +26,14 @@ func (S *KKRemoteService) HandleKKRemoteTask(a app.IApp, task *KKRemoteTask) err
 
 		v.To = task.Name + task.APITask.API()
 		v.Type = "text/json"
-		v.Content, _ = json.Marshal(task.APITask)
+		v.Content, _ = json.Encode(task.APITask)
 
 		var r = S.request(&v, task.Timeout)
 
 		if r == nil {
 			return errors.New("KKRemoteService request fail")
 		} else if r.Method == "REQUEST" && (r.Type == "text/json" || r.Type == "application/json") {
-			return json.Unmarshal(r.Content, task.APITask.GetResult())
+			return json.Decode(r.Content, task.APITask.GetResult())
 		} else {
 			return errors.New("KKRemoteService request fail " + r.String())
 		}
@@ -48,7 +48,7 @@ func (S *KKRemoteService) HandleKKRemoteTask(a app.IApp, task *KKRemoteTask) err
 func (S *KKRemoteService) HandleKKRemoteConnectTask(a app.IApp, task *KKRemoteConnectTask) error {
 
 	if S.request == nil {
-		S.request, S.getName = kk.TCPClientRequestConnect(task.Name, task.Address, task.Options)
+		S.request, S.getName, _ = kk.TCPClientRequestConnect(task.Name, task.Address, task.Options)
 	}
 
 	return nil
